2f0cce3abcb843ccbcd7f754def4be36b43a7221
Michael J. Ryan <tracker1@gmail.com>
2021-11-03 14:51:46 -0700

    Update depencencies, closes #17

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /* jshint node: true */
-'use strict';
+"use strict";
 
-var mapStream = require('map-stream');
-var template = require('./template');
+var mapStream = require("map-stream");
+var template = require("./template");
 
-var footerPlugin = function(footerText, data) {
-  footerText = footerText || '';
-  return mapStream(function(file, cb){
+var footerPlugin = function (footerText, data) {
+  footerText = footerText || "";
+  return mapStream(function (file, cb) {
     file.contents = Buffer.concat([
       file.contents,
-      new Buffer(template(footerText, Object.assign({file : file}, data)))
+      Buffer.from(template(footerText, Object.assign({ file: file }, data))),
     ]);
     cb(null, file);
   });
